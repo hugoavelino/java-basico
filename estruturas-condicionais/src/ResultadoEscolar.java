56f9ad75cf690a240147b15ecaa65f8aeca9070f
Anapiel <hugo.motta2010@gmail.com>
2024-06-24 17:00:14 -0300

    Fim estruturas condicionais

---
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,23 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+
+        int nota = 8;
+
+//        if(nota >= 7)
+//            System.out.println("Aprovado");
+//
+//        else if(nota >= 5 && nota < 7)
+//            System.out.println("Prova de recuperação");
+//
+//        else
+//            System.out.println("Reprovado");
+
+        /*String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);*/
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
+    }
+}
